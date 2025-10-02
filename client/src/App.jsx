54fef897c27dde03627cbbd97da13a9a89a47b65
aakarshit15 <saxenaakarshit15@gmail.com>
2025-10-02 22:31:14 +0530

    Upgrade Node Version

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,13 @@
-import { useState } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Simon_says from "./pages/games/simon_says/app.jsx";
 import "./App.css";
 
 function Home() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div className="card">
-        <Link to="/games/simon_says">Play Simon Says Game</Link>
+        <Link to="/games/simon_says" className="bg-red-500">Play Simon Says Game</Link>
       </div>
     </>
   );
