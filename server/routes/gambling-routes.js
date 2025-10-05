4eeddfd6407ced156524e176e9344f76eee3b0d2
Samyak-Vishrani <samyak.vishrani@gmail.com>
2025-10-05 23:03:52 +0530

    previous-backend-restored

--- server/routes/gambling-routes.js
+++ server/routes/gambling-routes.js
@@ -1,30 +1,30 @@
-import express from "express";
-import {
-  placeCamelBet,
-  declareCamelWinner,
-  getAllCamelBets,
-  placeAuctionBet,
-  revealAuctionResults,
-  getAllAuctionBets,
-  cancelActiveBet,
-  getActiveBetStatus,
-} from "../controllers/gamblingController.js";
-import { authenticateAdmin } from "../middleware/auth.js";
+// import express from "express";
+// import {
+//   placeCamelBet,
+//   declareCamelWinner,
+//   getAllCamelBets,
+//   placeAuctionBet,
+//   revealAuctionResults,
+//   getAllAuctionBets,
+//   cancelActiveBet,
+//   getActiveBetStatus,
+// } from "../controllers/gamblingController.js";
+// import { authenticateAdmin } from "../middleware/auth.js";
 
-const router = express.Router();
+// const router = express.Router();
 
-// Camel Race Routes
-router.post("/camel/bet", authenticateAdmin, placeCamelBet);
-router.post("/camel/declare-winner", authenticateAdmin, declareCamelWinner);
-router.get("/camel/bets", authenticateAdmin, getAllCamelBets);
+// // Camel Race Routes
+// router.post("/camel/bet", authenticateAdmin, placeCamelBet);
+// router.post("/camel/declare-winner", authenticateAdmin, declareCamelWinner);
+// router.get("/camel/bets", authenticateAdmin, getAllCamelBets);
 
-// Auction Routes
-router.post("/auction/bet", authenticateAdmin, placeAuctionBet);
-router.post("/auction/reveal", authenticateAdmin, revealAuctionResults);
-router.get("/auction/bets", authenticateAdmin, getAllAuctionBets);
+// // Auction Routes
+// router.post("/auction/bet", authenticateAdmin, placeAuctionBet);
+// router.post("/auction/reveal", authenticateAdmin, revealAuctionResults);
+// router.get("/auction/bets", authenticateAdmin, getAllAuctionBets);
 
-// Common Routes
-router.post("/cancel-bet", cancelActiveBet);
-router.get("/active-bet-status", getActiveBetStatus);
+// // Common Routes
+// router.post("/cancel-bet", cancelActiveBet);
+// router.get("/active-bet-status", getActiveBetStatus);
 
-export default router;
+// export default router;
